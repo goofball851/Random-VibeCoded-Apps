63586b7da85e166fc852611fe2dc1ad9db379fd8
goofball851 <goofball851@gmail.com>
2025-12-20 10:07:13 -0500

    Created fun-box.js

---
+++ FunBox/src/fun-box.js
@@ -0,0 +1,48 @@
+class FunBox extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    const type = this.getAttribute("type") || "placeholder";
+    const src = this.getAttribute("src");
+    const character = this.getAttribute("character") || "none";
+    const behavior = this.getAttribute("behavior") || "idle";
+    const trigger = this.getAttribute("trigger") || "click";
+
+    this.shadowRoot.innerHTML = `
+      <link rel="stylesheet" href="src/fun-box.css">
+      <div class="fun-box-wrapper" data-character="${character}">
+        <div class="fun-box-canvas" id="canvas"></div>
+        ${character !== "none" ? `<div class="mascot-face">${character}</div>` : ""}
+      </div>
+    `;
+
+    this.loadPlugin(type, src, behavior);
+    this.addTrigger(trigger, behavior);
+  }
+
+  addTrigger(trigger, behavior) {
+    this.shadowRoot.querySelector(".fun-box-wrapper")
+      .addEventListener(trigger, () => {
+        if (this.plugin && typeof this.plugin.play === "function") {
+          this.plugin.play(behavior);
+        } else {
+          console.log(`[FunBox] Triggered: ${behavior}`);
+        }
+      });
+  }
+
+  async loadPlugin(type, src, behavior) {
+    try {
+      const module = await import(`./plugins/loader-${type}.js`);
+      this.plugin = await module.default(this.shadowRoot.getElementById("canvas"), src, behavior);
+    } catch (e) {
+      console.warn(`[FunBox] Failed to load plugin "${type}". Using fallback.`);
+      this.shadowRoot.getElementById("canvas").innerHTML = `<img src="public/placeholder.svg" alt="FunBox Placeholder">`;
+    }
+  }
+}
+
+customElements.define('fun-box', FunBox);
