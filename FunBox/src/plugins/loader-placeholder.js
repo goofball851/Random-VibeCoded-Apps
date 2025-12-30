8fa2c2c9f8ef95e18bdbca883160d81e27a951b7
goofball851 <goofball851@gmail.com>
2025-12-30 07:38:38 -0500

    Updated loader-placeholder.js

--- FunBox/src/plugins/loader-placeholder.js
+++ FunBox/src/plugins/loader-placeholder.js
@@ -1,8 +1,13 @@
-export default async function(canvas, src, behavior) {
-  canvas.innerHTML = `<div style="text-align:center;">🔧 Placeholder<br><small>${behavior}</small></div>`;
+export default async function(container, src, behavior) {
+  container.innerHTML = `<div style="text-align:center;">🔧 Placeholder<br><small>${behavior}</small></div>`;
+
   return {
-    play: (action) => {
-      canvas.innerHTML = `<div style="text-align:center;">🎬 Playing:<br><strong>${action}</strong></div>`;
+    play: (state) => {
+      container.innerHTML = `<div style="text-align:center;">🎬 Playing:<br><strong>${state}</strong></div>`;
+    },
+    destroy: () => {
+      console.log("[FunBox] Destroying placeholder plugin.");
+      container.innerHTML = "";
     }
   };
 }
